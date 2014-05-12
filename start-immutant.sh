e08c6fa47da27c3573de38320e51c7cc2a0f42a3
Chris Shea <cmshea@gmail.com>
2014-05-12 16:55:07 -0400

    [CS NH] Correct the base directory for immutant

--- start-immutant.sh
+++ start-immutant.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cp /env-configs/immutant/$IMMUTANT_ENVIRONMENT/standalone.xml /immutant-$IMMUTANT_VERSION/jboss/standalone/configuration/standalone.xml
+export IMMUTANT_HOME="/immutant-$IMMUTANT_VERSION-slim"
+
+cp /env-configs/immutant/$IMMUTANT_ENVIRONMENT/standalone.xml $IMMUTANT_HOME/jboss/standalone/configuration/standalone.xml
 
 for DESCRIPTOR in /servers/*/*.clj; do
   BASENAME=$(basename $DESCRIPTOR)
-  cp $DESCRIPTOR /immutant-$IMMUTANT_VERSION/jboss/standalone/deployments/
-  touch /immutant-$IMMUTANT_VERSION/jboss/standalone/deployments/$BASENAME.dodeploy
+  cp $DESCRIPTOR $IMMUTANT_HOME/jboss/standalone/deployments/
+  touch $IMMUTANT_HOME/jboss/standalone/deployments/$BASENAME.dodeploy
 done
 
-/immutant-$IMMUTANT_VERSION/jboss/bin/standalone.sh -b 0.0.0.0
+$IMMUTANT_HOME/jboss/bin/standalone.sh -b 0.0.0.0
